cbb16bf37b87a84a81cc472479e59fc8ad027ea4
darshangera <68511550+1980x@users.noreply.github.com>
2021-01-28 11:33:46 +0530

    Update loss.py

--- algorithm/loss.py
+++ algorithm/loss.py
@@ -1,6 +1,6 @@
 '''
 Aum Sri Sai Ram
-Implementation of ECCT: Ensemble Consensual Collaborative Training for Facial Expression Recognition with Noisy Annotations Resnet models                
+Implementation of ECCT: Ensemble Consensual Collaborative Training for Facial Expression Recognition with Noisy Annotations           
 Authors: Darshan Gera and Dr. S. Balasubramanian, SSSIHL
 Date: 20-01-2021
 Email: darshangera@sssihl.edu.in
@@ -28,37 +28,16 @@ def kl_loss_compute(pred, soft_targets, reduce=True):
         return torch.sum(kl, 1)
 
 
-
-def loss_sai(y_1, y_2, t, co_lambda_max = 0.9, beta = 0.65, epoch_num = 1, max_epochs = 100): 
-    
+#Dynamic balancing of Suprevision Loss and Consistency Loss
+def ECCTloss(y_1, y_2, y_3, t, co_lambda_max = 0.9, beta = 0.65, epoch_num = 1, max_epochs = 40): 
+    ''' 
+        y_1, y_2, y_3 are predictions of 3 networks and t is target labels. 
+    '''
     e = epoch_num
       
     e_r = 0.9 * max_epochs
      
-    co_lambda = co_lambda_max * math.exp(-1.0 * beta * (1.0 - e / e_r ) ** 2) 
-     
-    loss_ce_1 = F.cross_entropy(y_1, t) 
-    
-    loss_ce_2 = F.cross_entropy(y_2, t) 
-    
-    loss_ce =   (1 - co_lambda) * 0.5 * (loss_ce_1 + loss_ce_2)
-    
-    loss_kl =    co_lambda * 0.5 * ( kl_loss_compute(y_1, y_2) +  kl_loss_compute(y_2, y_1))
-        
-    loss  =  (loss_kl + loss_ce).cpu() 
-     
-    
-    return loss
-
-
-    
-def loss_sai_extended(y_1, y_2, y_3, t, co_lambda_max = 0.9, beta = 0.65, epoch_num = 1, max_epochs = 100):
-
-    e = epoch_num
-      
-    e_r = 0.9 * max_epochs
-     
-    co_lambda = co_lambda_max * math.exp(-1.0 * beta * (1.0 - e / e_r ) ** 2) 
+    co_lambda = co_lambda_max * math.exp(-1.0 * beta * (1.0 - e / e_r ) ** 2) #Dynamic balancing factor using Gaussian like ramp-up function
      
     loss_ce_1 = F.cross_entropy(y_1, t) 
     
@@ -66,40 +45,11 @@ def loss_sai_extended(y_1, y_2, y_3, t, co_lambda_max = 0.9, beta = 0.65, epoch_
     
     loss_ce_3 = F.cross_entropy(y_3, t) 
     
-    loss_ce =   (1 - co_lambda) * 0.33 * (loss_ce_1 + loss_ce_2 + loss_ce_3)
+    loss_ce =   (1 - co_lambda) * 0.33 * (loss_ce_1 + loss_ce_2 + loss_ce_3)    #Supervision Loss weighted by (1 - dynamic balancing factor)
     
-    
-    consistencyLoss =  co_lambda * 0.33 * ( kl_loss_compute(y_1, y_2) +  kl_loss_compute(y_2, y_1)  + kl_loss_compute(y_1, y_3) +  kl_loss_compute(y_3, y_1) + kl_loss_compute(y_3, y_2) +  kl_loss_compute(y_2, y_3))        
+    consistencyLoss =  co_lambda * 0.33 * ( kl_loss_compute(y_1, y_2) +  kl_loss_compute(y_2, y_1)  + kl_loss_compute(y_1, y_3) +  kl_loss_compute(y_3, y_1) + kl_loss_compute(y_3, y_2) +  kl_loss_compute(y_2, y_3))  #Consistency Loss weighted by dynamic balancing factor
      
     loss  =  (consistencyLoss + loss_ce).cpu()
-    return loss 
-    
-def loss_sai_extended_4(y_1, y_2, y_3,y_4, t, co_lambda_max = 0.9, beta = 0.65, epoch_num = 1, max_epochs = 100):
-
-   
-        
-    e = epoch_num
-      
-    e_r = 0.9 * max_epochs
-     
-    co_lambda = co_lambda_max * math.exp(-1.0 * beta * (1.0 - e / e_r ) ** 2) # co_lambda_max * exp(-beta (1-e/e_r)^2) based on  Noisy Concurrent Training for Efficient Learning under Label Noise(WACV 2021)
-     
-    loss_ce_1 = F.cross_entropy(y_1, t) 
-    
-    loss_ce_2 =  F.cross_entropy(y_2, t)
-    
-    loss_ce_3 = F.cross_entropy(y_3, t) 
-    loss_ce_4 = F.cross_entropy(y_4, t) 
-    
-    
-    loss_ce =   (1 - co_lambda) * (1/4.0) * (loss_ce_1 + loss_ce_2 + loss_ce_3+ loss_ce_4)
-    
-    
-    consistencyLoss =  co_lambda * (1/6.0) * ( js_loss_compute(y_1, y_2) +  js_loss_compute(y_1, y_3)  + js_loss_compute(y_1, y_4) +  js_loss_compute(y_2, y_3) + js_loss_compute(y_2, y_4) +  js_loss_compute(y_3, y_4))
-        
-   
-    loss  =  (  consistencyLoss + loss_ce).cpu()
-       
     
     return loss     
     
